aa10957950ac0aaea7ed2afe4f578fc6b12b4c1b
Markus Honkala <markus.honkala@tuni.fi>
2025-12-07 03:55:32 +0200

    Fixed issue in 2 tests in defaultToAny

--- tests/defaultToAny.test.js
+++ tests/defaultToAny.test.js
@@ -2,12 +2,12 @@ import defaultToAny from '../src/defaultToAny.js'
 
 describe ('defaultToAny', () => {
     test('should return the first value when no value is not null/undefined/Nan', () => {
-        expect(defaultToAny(1,2,3)).toBe([1])
+        expect(defaultToAny(1,2,3)).toBe(1)
         expect(defaultToAny("bread", "milk")).toBe("bread")
     })
 
     test('should return the first value which is not null/undefined/Nan', () => {
-        expect(defaultToAny(null,undefined,3)).toBe([3])
+        expect(defaultToAny(null,undefined,3)).toBe(3)
         expect(defaultToAny(NaN, "milk")).toBe("milk")
     })    
     
